5f3e118186f2202aa33e583dd55c32ed281cc4ed
Juvid <jutewei@qq.com>
2018-05-08 10:31:50 +0800

    枚举

---
+++ JuAllTest/JuENUM/JuEnum.h
@@ -0,0 +1,13 @@
+//
+//  JuEnum.h
+//  JuAllTest
+//
+//  Created by Juvid on 2018/5/8.
+//  Copyright © 2018年 Juvid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JuEnum : NSObject
+
+@end
---
+++ JuAllTest/JuENUM/JuEnum.m
@@ -0,0 +1,13 @@
+//
+//  JuEnum.m
+//  JuAllTest
+//
+//  Created by Juvid on 2018/5/8.
+//  Copyright © 2018年 Juvid. All rights reserved.
+//
+
+#import "JuEnum.h"
+
+@implementation JuEnum
+
+@end
